e206ce2257ffcedff6c35754e0c5e41b3562623f
ZalGe <61461110+ZalGe@users.noreply.github.com>
2020-10-04 15:00:13 +0200

    update

--- main.c
+++ main.c
@@ -9,18 +9,14 @@ int main() {
     double i = 1;
     int n = 0;
 
-
-    while (NextSequence > pow(10, -11))
+    while (NextSequence > (pow(10, -11)))
     {
-        NextSequence =/*NextSequence*/((pow(2*i,2))/((pow(2*i,2))-1));
+        NextSequence = (2*2*i*i)/((2*2*i*i)-1);
         pi = (pi * NextSequence);
         Pi = 2*pi;
         i = i + 1;
         n = n + 1;
         printf("%d -> %.17f %.10f\n",n,NextSequence,Pi);
-        //NextSequence=1;
     }
-
-
     return 0;
 }
